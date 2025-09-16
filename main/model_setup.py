3a10fae4b25aa83cf9591e3e8f41c695df680ab8
aashish <aashish.tws@gmal.com>
2025-09-16 11:04:50 +0530

    quaantize the  model

--- main/model_setup.py
+++ main/model_setup.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 
 # model_path = "launchco/eb3-llm-health/tree/main/eb3-health"
@@ -22,9 +22,18 @@ def tokenize_data(model_path="launchco/eb3-llm-health"):
 # print('tokenizr--->>>')
 # Load model
 def load_model(model_path="launchco/eb3-llm-health"):
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,              # Enable 4-bit
+        bnb_4bit_compute_dtype=torch.float16,  # Can try torch.bfloat16 if your GPU supports it
+        bnb_4bit_use_double_quant=True, # Nested quantization for efficiency
+        bnb_4bit_quant_type="nf4"       # NormalFloat4 (better than fp4 usually)
+    )
+
     model = AutoModelForCausalLM.from_pretrained(
         model_path,
         subfolder="eb3-health",
+        quantization_config=bnb_config,
         device_map="auto",
         torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
         trust_remote_code=True
